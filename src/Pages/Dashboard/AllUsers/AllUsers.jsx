ba5a267fde7216a45c801e3841da9b3c522b8625
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-06-10 20:13:24 +0600

    admin role added

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,8 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import { GrUserAdmin } from "react-icons/gr";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
+
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await fetch('http://localhost:5000/users')
+        return res.json();
+    })
+
+    const handleMakeAdmin = (user) => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is an Admin Now..!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    }
+
     return (
         <div>
-            <h2>All Users</h2>
+            <h2 className="text-2xl font-bold my-8">Total Users: {users.length}</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <td>{index + 1}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role === 'admin' ? 'admin' :
+                                    <button onClick={()=> handleMakeAdmin(user)} className="btn btn-ghost bg-rose-400"><GrUserAdmin size={20}/></button>}
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
