5ec9c90f27628425133f54917cd4c21d9d7ab8b1
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-06-12 16:21:15 +0600

    payment check out form added

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,10 +1,24 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ price }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const { user } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
     const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('');
+    const [processing, setProcessing] = useState(false);
+
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', { price })
+            .then(res => {
+                setClientSecret(res.data.clientSecret);
+            })
+    }, [])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -26,12 +40,36 @@ const CheckOutForm = () => {
             setCardError(error.message);
         }
         else {
-            console.log('payment: ', paymentMethod);
+            console.log('payment method :', paymentMethod);
             setCardError('');
+        }
+        setProcessing(true);
+
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: user?.displayName || 'anonymous',
+                        email: user?.email || 'anonymous'
+                    }
+                }
+            }
+        )
+        if (confirmError) {
+            console.log(confirmError);
+        }
+        console.log('payment intent :', paymentIntent);
+        setProcessing(false);
+        if (paymentIntent.status === 'succeeded') {
+            setTransactionId(paymentIntent.id)
+
         }
     }
 
     return (
+        <>
             <form className="w-1/2 mx-auto mt-12" onSubmit={handleSubmit}>
                 <CardElement
                     options={{
@@ -49,13 +87,18 @@ const CheckOutForm = () => {
                         },
                     }}
                 />
-                {cardError && <p className="text-red-500 text-center">{cardError}</p>}
+
                 <div className="mt-10 text-center">
-                    <button type="submit" className="btn bg-rose-300 btn-sm" disabled={!stripe}>
+                    <button type="submit" className="btn bg-rose-300 btn-sm"
+                        disabled={!stripe || !clientSecret || processing}>
                         Payment
                     </button>
                 </div>
             </form>
+
+            {cardError && <p className="text-red-500 text-center mt-10">{cardError}</p>}
+            {transactionId && <p className="text-green-500 text-center mt-10">Transaction Complete With Transaction Id: {transactionId}</p>}
+        </>
     );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,15 +1,19 @@
 import { loadStripe } from "@stripe/stripe-js";
 import CheckOutForm from "./CheckOutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useSelectClass from "../../../hooks/useSelectClass";
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
+    const [cart] = useSelectClass();
+    const total = cart.reduce( (sum, item) => sum + item.price, 0)
+    const price = parseFloat(total.toFixed(2))
     return (
         <div>
             <h2 className="text-3xl font-bold text-center mt-8 mb-4">Payment</h2>
 
             <Elements stripe={stripePromise}>
-                <CheckOutForm />
+                <CheckOutForm price={price} />
             </Elements>
         </div>
     );
